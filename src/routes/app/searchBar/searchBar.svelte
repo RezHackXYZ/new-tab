30122622cfbfe1d0575a18320ad2e7036b31c9d5
RezHackXYZ <rhythmupa@gmail.com>
2025-07-03 14:50:46 -0700

    fdsfd

--- src/routes/app/searchBar/searchBar.svelte
+++ src/routes/app/searchBar/searchBar.svelte
@@ -2,15 +2,18 @@
 	import Logo from "./logo.svelte";
 </script>
 
-<label
+<div
 	id="bar"
-	class="flex items-center justify-center rounded-xl border-4 border-blue-400 bg-blue-300 px-5 py-2 has-[:focus]:border-blue-600"
+	class="flex items-center justify-center rounded-xl border-4 border-blue-400 bg-blue-300 px-5 py-2 transition has-[:focus]:-translate-y-7 has-[:focus]:scale-120 has-[:focus]:border-blue-600"
 >
 	<Logo />
 	<input
 		id="bar"
 		type="text"
-		class="w-[50vw] rounded-lg px-4 py-2 text-xl text-blue-500 focus:outline-none"
+		class="peer z-30 w-[50vw] rounded-lg px-4 py-2 text-xl text-blue-500 focus:outline-none"
 		placeholder="Search Some Shit..."
 	/>
-</label>
+	
+	<div class="fixed top-0 left-0 z-20 hidden h-full w-full backdrop-blur-2xl peer-focus:block bg-black"></div>
+</div>
+
--- src/routes/app/settings/baground.svelte
+++ src/routes/app/settings/baground.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <button
-	class="fixed top-0 left-0 z-20 h-screen w-screen backdrop-blur-xl {$IsSettingOpen
-		? 'hidden'
-		: 'visible'}"
+	class="fixed top-0 left-0 z-20 h-screen w-screen backdrop-blur-xl transition {$IsSettingOpen
+		? 'hidden opacity-0'
+		: 'visible opacity-100'}"
 	onclick={() => IsSettingOpen.set(!$IsSettingOpen)}
 	aria-label="close settings"
 ></button>
